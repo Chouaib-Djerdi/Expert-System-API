c988690f92fc302028b94afe6837a58a90f64a06
Chouaib-Djerdi <djerdichouaib@gmail.com>
2024-04-20 15:33:31 +0100

    data.json

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import ProblemListView, SignListView, DiagnosisView, SingleSignView
+from .views import ProblemListView, SignListView, DiagnosisView
 
 urlpatterns = [
     path("problems/", ProblemListView.as_view(), name="problems-list"),
     path("signs/", SignListView.as_view(), name="signs-list"),
     path("diagnosis/", DiagnosisView.as_view(), name="diagnosis"),
-    path("sign/", SingleSignView.as_view(), name="single-sign"),
 ]
